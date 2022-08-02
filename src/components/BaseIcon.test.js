5beec39078377371fc0e2f13c7efd15cc76dfc01
Dmitry Verteyko <verteyko1990@gmail.com>
2022-08-02 13:49:22 +0300

    added baseIcon test

--- src/components/BaseIcon.test.js
+++ src/components/BaseIcon.test.js
@@ -21,3 +21,33 @@ test('renders base icon no-exist icon', () => {
     const component = screen.getByTestId('base-icon');
     expect(component.innerHTML).toBeFalsy();
 })
+
+
+test('rendes base icon with default classes', () => {
+    const defaultClasses = 'w-6 h-6';
+
+    const options = {
+        props: {
+            name: 'menu',
+        },
+    };
+
+    render(BaseIcon, options);
+    const component = screen.getByTestId('base-icon');
+    expect(component.getAttribute('class')).toBe(defaultClasses);
+});
+
+test('rendes base icon with custom classes', () => {
+    const classes = 'w-10 h-100';
+
+    const options = {
+        props: {
+            name: 'menu',
+            class: classes,
+        },
+    };
+
+    render(BaseIcon, options);
+    const component = screen.getByTestId('base-icon');
+    expect(component.getAttribute('class')).toBe(classes);
+});
