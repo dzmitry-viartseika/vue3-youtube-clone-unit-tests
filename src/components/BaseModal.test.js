6478807b6ea395e8792cd22295362a2629004d4d
Dmitry Verteyko <verteyko1990@gmail.com>
2022-08-03 12:25:11 +0300

    added BaseModal test

--- src/components/BaseModal.test.js
+++ src/components/BaseModal.test.js
@@ -1,4 +1,4 @@
-import { screen, render } from '@testing-library/vue';
+import { screen, render, fireEvent } from '@testing-library/vue';
 import BaseModal from './BaseModal.vue';
 import icons from '../icons.js';
 
@@ -34,11 +34,31 @@ test.only('renders close button with conditional', () => {
     expect(screen.getByTestId('base-icon').innerHTML).toBe(icons['x']);
 })
 
-test.only('renders close button without conditional', () => {
+test('renders close button without conditional', () => {
     render(BaseModal);
 
     screen.debug();
     // get для получения
     // query если его нет
     expect(screen.queryByTestId('base-icon').innerHTML).toBeNull();
+})
+
+test.only('modal form should close after click close icon', async () => {
+    const bodySlot = 'This is slot of body';
+
+    const options = {
+        props: {
+            withCloseButton: true,
+        },
+        slots: {
+            default: bodySlot,
+        }
+    }
+
+    render(BaseModal, options);
+
+    const button = screen.getByTestId('base-modal-button-close');
+    await fireEvent.click(button);
+    expect(screen.queryByText(bodySlot)).toBeNull();
+    expect(screen.queryByTestId('base-modal-overlay')).toBeNull();
 })
