090e71df63edd04ac4320b281d6a2ae3925c14e0
Dzmitry Viartseika <dviartseika@exadel.com>
2022-08-12 12:13:05 +0200

    refactoring code TheModalSearchPredictions.test.js

--- src/components/TheModalSearchPredictions.test.js
+++ src/components/TheModalSearchPredictions.test.js
@@ -1,6 +1,20 @@
 import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/vue';
 import TheModalSearchPredictions from './TheModalSearchPredictions';
 
+// вынести определение данных в общую describe - можно сгруппировать схожие сценарии
+
+const searchPredictions = [
+    'Search Prediction 1',
+    'Search Prediction 2',
+    'Search Prediction 3',
+];
+
+const categories = [
+    'Hateful',
+    'Sexually Explicit',
+    'Violent',
+];
+
 function renderModal(searchPredictions = [], searchPredictionCategories = []) {
     const options = {
         props: {
@@ -14,61 +28,43 @@ function renderModal(searchPredictions = [], searchPredictionCategories = []) {
     return render(TheModalSearchPredictions, options);
 }
 
-it('shows search prediction list', () => {
-    // arrange
-    const searchPredictions = [
-        'Search Prediction 1',
-        'Search Prediction 2',
-        'Search Prediction 3',
-    ];
 
-    renderModal(searchPredictions);
+describe('when open', () => {
+    it('shows search prediction list', () => {
+        renderModal(searchPredictions);
 
-    searchPredictions.forEach((prediction) => screen.getByText(prediction));
+        searchPredictions.forEach((prediction) => screen.getByText(prediction));
 
-    // or
+        // or
 
-    // screen.getByText(searchPredictions[0])
-    // screen.getByText(searchPredictions[1])
-    // screen.getByText(searchPredictions[2])
+        // screen.getByText(searchPredictions[0])
+        // screen.getByText(searchPredictions[1])
+        // screen.getByText(searchPredictions[2])
+    })
+
+    it('shows search prediction category', () => {
+        renderModal(categories);
+
+        categories.forEach((category) => screen.getByText(category));
+    });
 })
 
-it('shows search prediction category', () => {
-    const categories = [
-        'Hateful',
-        'Sexually Explicit',
-        'Violent',
-    ];
 
-    renderModal(categories);
 
-    categories.forEach((category) => screen.getByText(category));
-});
+describe('when close' , () => {
+    it('does not show search predictions if closed', () => {
+        renderModal(searchPredictions);
 
-it('does not show search predictions if closed', () => {
-    const searchPredictions = [
-        'Search Prediction 1',
-        'Search Prediction 2',
-        'Search Prediction 3',
-    ];
+        const button = screen.getByRole('button', {name: 'Cancel'});
+        fireEvent.click(button);
+        return waitForElementToBeRemoved(searchPredictions.map((prediction) => screen.queryByText(prediction)));
+    })
 
-    renderModal(searchPredictions);
+    it('does not show search predictions categories if closed', () => {
+        renderModal(categories);
 
-    const button = screen.getByRole('button', {name: 'Cancel'});
-    fireEvent.click(button);
-    return waitForElementToBeRemoved(searchPredictions.map((prediction) => screen.queryByText(prediction)));
-})
-
-it('does not show search predictions categories if closed', () => {
-    const categories = [
-        'Hateful',
-        'Sexually Explicit',
-        'Violent',
-    ];
-
-    renderModal(categories);
-
-    const button = screen.getByRole('button', {name: 'Cancel'});
-    fireEvent.click(button);
-    return waitForElementToBeRemoved(categories.map((category) => screen.queryByText(category)));
+        const button = screen.getByRole('button', {name: 'Cancel'});
+        fireEvent.click(button);
+        return waitForElementToBeRemoved(categories.map((category) => screen.queryByText(category)));
+    })
 })
