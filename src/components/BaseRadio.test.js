1891dc17c17c6b953a0d35e73800c3d2df27744e
Dzmitry Viartseika <dviartseika@exadel.com>
2022-08-12 14:58:44 +0200

    refactoring code BaseTextarea.test.js

---
+++ src/components/BaseRadio.test.js
@@ -0,0 +1,46 @@
+import { render, screen, } from '@testing-library/vue';
+import BaseRadio from './BaseRadio';
+
+const label = 'Label';
+const isChecked = true;
+const value = 'value';
+const id = 'id';
+
+function renderRadio(isChecked = false) {
+
+    const options = {
+        props: {
+            id,
+            modelValue: isChecked ? value : null,
+            value,
+        },
+        slots: {
+            default: 'Label',
+        },
+    }
+
+    return render(BaseRadio, options);
+};
+
+// it.only('consists id radio button', () => {
+//     renderRadio();
+//     screen.debug();
+//     screen.getByAltText('test-id-1')
+// })
+
+it('renders checked with label', () => {
+    const isChecked = true;
+    renderRadio(isChecked);
+    expect(screen.getByLabelText(label).checked).toBe(isChecked);
+})
+
+it('renders unchecked with label', () => {
+    const isChecked = false;
+    renderRadio(isChecked);
+    expect(screen.getByLabelText(label).checked).toBe(isChecked);
+})
+
+it('renders with value', () => {
+    renderRadio();
+    expect(screen.getByLabelText(label).value).toBe(value);
+})
---
+++ src/components/BaseTextarea.test.js
@@ -0,0 +1,27 @@
+import {render, screen, fireEvent, waitForElementToBeRemoved, getByAltText} from '@testing-library/vue';
+import BaseTextarea from './BaseTextarea';
+
+const text = 'text';
+
+function RenderBaseTextArea(limit = 0) {
+    const options = {
+        props: {
+            modelValue: text,
+            limit,
+        }
+    }
+
+    return render(BaseTextarea, options);
+}
+
+it('renders prefilled', () => {
+    RenderBaseTextArea(200);
+    screen.debug();
+    expect(screen.getByDisplayValue(text));
+});
+
+it('shows counters', () => {
+    const limit = 200;
+    RenderBaseTextArea(limit);
+    expect(screen.getByText(`${text.length} / ${limit}`));
+});
