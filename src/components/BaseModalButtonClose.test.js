e25b9c2a0b1016bb415349d05a1cad83cda98948
Dzmitry Viartseika <dviartseika@exadel.com>
2022-08-16 10:13:07 +0200

    added snapshot BaseModalOverlay.test.js.snap

---
+++ src/components/BaseModalButtonClose.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/vue';
+import BaseModalButtonClose from './BaseModalButtonClose.vue';
+
+it('renders', () => {
+    const { html } = render(BaseModalButtonClose);
+    expect(html()).toMatchSnapshot();
+})
--- src/components/BaseModalOverlay.test.js
+++ src/components/BaseModalOverlay.test.js
@@ -3,5 +3,17 @@ import BaseModalOverlay from './BaseModalOverlay';
 
 it.only('renders with snapshot', () => {
     const { html } = render(BaseModalOverlay);
+    const comment = 'A very very long comment';
+    const videoObj = {
+        id: Math.ceil(Math.random() * 100),
+        title: 'Video title',
+        publishedAt: new Date(),
+
+    }
     expect(html()).toMatchSnapshot();
+    expect(videoObj).toMatchSnapshot({
+        id: expect.any(Number),
+        publishedAt: expect.any(Date)
+    });
+    expect(comment).toMatchSnapshot();
 });
